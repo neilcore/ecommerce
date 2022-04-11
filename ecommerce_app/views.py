803896af1d30f6bb2fa4053287331b5a0b7ed957
Neil Brian Temblor <neilcore22@gmail.com>
2022-04-11 11:12:25 -0400

    saving updates-item-view-template

--- ecommerce_app/views.py
+++ ecommerce_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def home(request):
     
     return render(request, "ecommerce_app/home/home.html")
+
+def viewItem(request):
+    
+    return render(request, "ecommerce_app/product_view/viewItem.html")
